0301629fc80747b212fc79d92b92b790355df607
Ashirwad Dwivedi <ashirwaddwi5510@gmail.com>
2025-09-08 12:51:09 +0000

    restarted

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
-import React from "react";
-import PaymentLock from "./components/PaymentLock";
+// App.jsx
+import React, { useState } from "react";
+import LogoSplash from "./components/LogoSplash";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Team from "./components/OurTeam";
+import Membership from "./components/Membership";
+import Footer from "./components/Footer";
+import Faqs from './components/Faqs'
+export default function App() {
+  const [showSplash, setShowSplash] = useState(true);
 
-function App() {
-  return <PaymentLock />;
+  return (
+    <>
+      {showSplash ? (
+        <LogoSplash onFinish={() => setShowSplash(false)} />
+      ) : (
+        <>
+          <Navbar />
+          <Hero />
+          <About />
+          <Team />
+          <Faqs />
+          <Membership />
+          <Footer />
+        </>
+      )}
+    </>
+  );
 }
-
-export default App;
